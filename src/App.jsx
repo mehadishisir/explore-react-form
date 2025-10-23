010252feb1574054cbc303fab7a27269f98c14d6
mehadi <mehadishisir@gmail.com>
2025-10-23 19:01:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import SimpleForm from "./components/simpleForm/SimpleForm";
+
+function App() {
+  return (
+    <>
+      <h1>Explore react Form</h1>
+      <SimpleForm></SimpleForm>
+    </>
+  );
+}
+
+export default App;
